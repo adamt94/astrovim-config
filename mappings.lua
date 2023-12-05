44df0cd63058250b41ec201ea0d700f69259ea61
adamt94 <thompson.ad94@gmail.com>
2023-12-05 18:41:35 +0000

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -26,14 +26,21 @@ return {
       desc = "Toggle Terminal",
     },
 
-    -- Harpoon
-    ["<leader>1"] = { function() require("harpoon.ui").nav_file(1) end, desc = "Goto mark 1" },
-    ["<leader>2"] = { function() require("harpoon.ui").nav_file(2) end, desc = "Goto mark 2" },
-    ["<leader>3"] = { function() require("harpoon.ui").nav_file(3) end, desc = "Goto mark 3" },
-    ["<leader>4"] = { function() require("harpoon.ui").nav_file(4) end, desc = "Goto mark 4" },
-    ["<leader>5"] = { function() require("harpoon.ui").nav_file(5) end, desc = "Goto mark 5" },
-    ["<leader>6"] = { function() require("harpoon.ui").nav_file(6) end, desc = "Goto mark 6" },
-    ["<leader>7"] = { function() require("harpoon.ui").nav_file(6) end, desc = "Goto mark 6" },
+   -- Harpoon
+    ["<leader><leader>1"] = { function() require("harpoon.ui").nav_file(1) end, desc = "Goto mark 1" },
+    ["<leader><leader>2"] = { function() require("harpoon.ui").nav_file(2) end, desc = "Goto mark 2" },
+    ["<leader><leader>3"] = { function() require("harpoon.ui").nav_file(3) end, desc = "Goto mark 3" },
+    ["<leader><leader>4"] = { function() require("harpoon.ui").nav_file(4) end, desc = "Goto mark 4" },
+    ["<leader><leader>5"] = { function() require("harpoon.ui").nav_file(5) end, desc = "Goto mark 5" },
+    ["<leader><leader>6"] = { function() require("harpoon.ui").nav_file(6) end, desc = "Goto mark 6" },
+    ["<leader><leader>7"] = { function() require("harpoon.ui").nav_file(6) end, desc = "Goto mark 6" },
+
+    -- Terminal switch
+    ["<leader>1"] = { function() require("toggleterm").exec("", 1) end, desc = "GoTo term 1" },
+    ["<leader>2"] = { function() require("toggleterm").exec("", 2) end, desc = "GoTo term 2" },
+    ["<leader>3"] = { function() require("toggleterm").exec("", 3) end, desc = "GoTo term 3" },
+    ["<leader>4"] = { function() require("toggleterm").exec("", 4) end, desc = "GoTo term 4" },
+
 
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
